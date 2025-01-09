c1f375418940ac0afc63ccb7c74438b3e8fb210b
Khagendra Neupane <nkhagendra1@gmail.com>
2025-01-10 05:16:20 +0545

    Update test.py

--- test.py
+++ test.py
@@ -1,13 +1,1 @@
-from gtts import gTTS
-import terminal
 
-def speak(text):
-    if not text.strip():  # Check if text is empty
-        print("No text to speak")
-        return
-    tts = gTTS(text=text, lang='en')
-    tts.save("response.mp3")
-    playsound.playsound("response.mp3")
-    os.remove("response.mp3")
-    
-speak("Hello sir, I'm samantha ")
